94fc4113a225715bd897e75c7df8adc4ed2030a1
Jabbar <angela.jabbar2@gmail.com>
2019-03-07 03:02:37 -0500

    attempted to fix the crash problem and create event

--- app/src/main/java/systemsonweb/com/planner/CreateEvent.java
+++ app/src/main/java/systemsonweb/com/planner/CreateEvent.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -17,7 +18,7 @@ public class CreateEvent extends AppCompatActivity {
     String sched;
     Button submit;
     TextView eventname,duedate,hours,sessions;
-    Queue<String> qe=new LinkedList<String>();
+    Queue<String> qe=new LinkedList<>();
 
 
     @Override
@@ -36,7 +37,16 @@ public class CreateEvent extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(CreateEvent.this,setSched(),Toast.LENGTH_LONG).show();
-                qe.add(sched);
+                Intent i = new Intent(CreateEvent.this,Schedule.class);
+                qe.add(setSched());
+                Iterator iterator = qe.iterator();
+                while(iterator.hasNext()){
+                    String element = (String) iterator.next();
+                    i.putExtra("Queue",element);
+                    startActivity(i);
+                    finish();
+                }
+
 
             }
         });
--- app/src/main/java/systemsonweb/com/planner/Schedule.java
+++ app/src/main/java/systemsonweb/com/planner/Schedule.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.LinkedList;
@@ -12,24 +13,21 @@ import java.util.Queue;
 
 public class Schedule extends AppCompatActivity {
 
-    Queue<String> qe=new LinkedList<String>();
+    TextView textView;
+    String stringg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
-        CreateEvent createEvent = new CreateEvent();
-        if(qe.isEmpty()){
-            Toast.makeText(this,createEvent.sched,Toast.LENGTH_LONG).show();
-        }
-        qe.add(createEvent.setSched());
-        for(int i=0;i<qe.size();i++){
-            LinearLayout layout = findViewById(R.id.schedlayout);
-            Button btn = new Button(this);
-            btn.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT));
-            btn.setText(qe.poll());
-            layout.addView(btn);
 
+        textView = findViewById(R.id.tv);
+
+        stringg=getIntent().getExtras().getString("Queue");
+        if(textView==null) {
+            textView.setText(stringg);
+        }else {
+            textView.setText(textView.getText()+"\n"+stringg);
         }
     }
 }
